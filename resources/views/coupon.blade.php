00997da406e5130716206691f13e8decfa0187e2
SwapnaliSinalkar <developer2@ottoedge.com>
2023-06-23 19:10:26 +0530

    thank you page add

---
+++ resources/views/coupon.blade.php
@@ -0,0 +1,41 @@
+@extends('main')
+
+@section('title', 'Reward')
+
+@section('style')
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+<style>
+    .coin-img{
+        width: 100px;
+    }
+    .bg-image{
+        background-image: url();
+    }
+    .content .head-title{
+        margin-top: 30%;
+    }
+</style>
+@endsection
+
+@section('content')
+<div class="bg rewardbg">
+    <div class="content">
+        @if(\Session::get('status')=='success')
+        <h1 class="head-title animate__animated animate__bounceIn">YOU WON</h1>
+        
+        <p class="cash-text animate__animated animate__bounceInUp">Thank you ! Cashback will be created withtin 24 Hours.</p>
+        @else
+        <script type="text/javascript">
+            window.location.href = "{{ route('logout') }}";
+        </script>
+        @endif
+    </div>
+</div>
+@endsection
+
+@section('script')
+
+@endsection
