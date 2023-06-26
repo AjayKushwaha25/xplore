9d3bf218199d98e136a036407b6eb08bae43438f
SwapnaliSinalkar <developer2@ottoedge.com>
2023-06-26 13:59:50 +0530

    new controller created for lp retailers

---
+++ app/Http/Requests/StoreLpRetailerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLpRetailerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|required',
+            'mobile_number' => 'bail|required|numeric|digits:10',
+            'whatsapp_number' => 'bail|required|numeric|digits:10',
+            'upi_id' => 'bail|required|unique:lp_register,upi_id',
+        ];
+    }
+}
