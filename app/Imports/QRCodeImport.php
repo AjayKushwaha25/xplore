1322f8e203e8aec0de1712e1836ff5788983537e
AjayKushwaha25 <ajay@ottoedge.com>
2023-06-27 18:44:26 +0530

    GCS Driver added

--- app/Imports/QRCodeImport.php
+++ app/Imports/QRCodeImport.php
@@ -230,7 +230,7 @@ class QRCodeImport implements ToCollection, WithValidation, WithStartRow, WithCh
     }
     public function chunkSize(): int
     {
-        return 200;
+        return 30;
     }
 
 }
